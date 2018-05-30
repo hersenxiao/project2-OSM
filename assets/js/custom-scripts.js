f14939243ea3893fa81125efd783fdc1380d0667
hersenxiao <120424206@qq.com>
2018-05-30 10:56:07 +0200

    tips

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -20,141 +20,7 @@
                     $('div.sidebar-collapse').removeClass('collapse')
                 }
             });
-
-            /* MORRIS BAR CHART
-			-----------------------------------------*/
-            Morris.Bar({
-                element: 'morris-bar-chart',
-                data: [{
-                    y: '2006',
-                    a: 100,
-                    b: 90
-                }, {
-                    y: '2007',
-                    a: 75,
-                    b: 65
-                }, {
-                    y: '2008',
-                    a: 50,
-                    b: 40
-                }, {
-                    y: '2009',
-                    a: 75,
-                    b: 65
-                }, {
-                    y: '2010',
-                    a: 50,
-                    b: 40
-                }, {
-                    y: '2011',
-                    a: 75,
-                    b: 65
-                }, {
-                    y: '2012',
-                    a: 100,
-                    b: 90
-                }],
-                xkey: 'y',
-                ykeys: ['a', 'b'],
-                labels: ['Series A', 'Series B'],
-				 barColors: [
-    '#e96562','#414e63',
-    '#A8E9DC' 
-  ],
-                hideHover: 'auto',
-                resize: true
-            });
-	 
-
-
-            /* MORRIS DONUT CHART
-			----------------------------------------*/
-            Morris.Donut({
-                element: 'morris-donut-chart',
-                data: [{
-                    label: "Profits",
-                    value: 12
-                }, {
-                    label: "Users",
-                    value: 30
-                }, {
-                    label: "Total Sales",
-                    value: 20
-                }],
-				   colors: [
-    '#A6A6A6','#414e63',
-    '#e96562' 
-  ],
-                resize: true
-            });
-
-            /* MORRIS AREA CHART
-			----------------------------------------*/
-
-            Morris.Area({
-                element: 'morris-area-chart',
-                data: [{
-                    period: '2010 Q1',
-                    iphone: 2666,
-                    ipad: null,
-                    itouch: 2647
-                }, {
-                    period: '2010 Q2',
-                    iphone: 2778,
-                    ipad: 2294,
-                    itouch: 2441
-                }, {
-                    period: '2010 Q3',
-                    iphone: 4912,
-                    ipad: 1969,
-                    itouch: 2501
-                }, {
-                    period: '2010 Q4',
-                    iphone: 3767,
-                    ipad: 3597,
-                    itouch: 5689
-                }, {
-                    period: '2011 Q1',
-                    iphone: 6810,
-                    ipad: 1914,
-                    itouch: 2293
-                }, {
-                    period: '2011 Q2',
-                    iphone: 5670,
-                    ipad: 4293,
-                    itouch: 1881
-                }, {
-                    period: '2011 Q3',
-                    iphone: 4820,
-                    ipad: 3795,
-                    itouch: 1588
-                }, {
-                    period: '2011 Q4',
-                    iphone: 15073,
-                    ipad: 5967,
-                    itouch: 5175
-                }, {
-                    period: '2012 Q1',
-                    iphone: 10687,
-                    ipad: 4460,
-                    itouch: 2028
-                }, {
-                    period: '2012 Q2',
-                    iphone: 8432,
-                    ipad: 5713,
-                    itouch: 1791
-                }],
-                xkey: 'period',
-                ykeys: ['iphone', 'ipad', 'itouch'],
-                labels: ['iPhone', 'iPad', 'iPod Touch'],
-                pointSize: 2,
-                hideHover: 'auto',
-				  pointFillColors:['#ffffff'],
-				  pointStrokeColors: ['black'],
-				  lineColors:['#A6A6A6','#414e63'],
-                resize: true
-            });
-
+          
             /* MORRIS LINE CHART
 			----------------------------------------*/
             Morris.Line({
@@ -191,9 +57,7 @@
             $('.bar-chart').cssCharts({type:"bar"});
             $('.donut-chart').cssCharts({type:"donut"}).trigger('show-donut-chart');
             $('.line-chart').cssCharts({type:"line"});
-
             $('.pie-thychart').cssCharts({type:"pie"});
-       
 	 
         },
 
@@ -227,3 +91,8 @@
 	$(".dropdown-button").dropdown();
 	
 }(jQuery));
+
+function GetValueForToolTip(obj) {
+    var tValue = "load a file";
+    obj.setAttribute("title", tValue);
+}
